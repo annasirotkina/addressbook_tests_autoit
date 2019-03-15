87fcd9348c8b17bac6fb401703064bdf9a481c5f
annasirotkina <anna.sir21@gmail.com>
2019-03-15 17:28:40 +0300

    Десктоп

---
+++ addressbook_tests_autoit/model/GroupData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace addressbook_tests_autoit
+{
+    public class GroupData : IComparable<GroupData>, IEquatable<GroupData>
+    {
+        public string Name { get; set; }
+
+        public int CompareTo(GroupData other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+
+        public bool Equals(GroupData other)
+        {
+            if (Object.ReferenceEquals(other, null))
+            {
+                return false;
+            }
+            if (Object.ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return Name == other.Name;
+        }
+    }
+}
---
+++ addressbook_tests_autoit/tests/GroupRemovalTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace addressbook_tests_autoit
+{
+    [TestFixture]
+    public class GroupRemovalTests : TestBase
+    {
+        [Test]
+        public void TestGroupRemoval()
+        {
+            List<GroupData> oldGroups = app.Groups.GetGroupList();
+            GroupData removedGroup = oldGroups[0];
+
+            app.Groups.Remove(removedGroup);
+
+            List<GroupData> newGroups = app.Groups.GetGroupList();
+            oldGroups.RemoveAt(0);
+            oldGroups.Sort();
+            newGroups.Sort();
+            Assert.AreEqual(oldGroups, newGroups);
+
+        }
+
+    }
+
+}
